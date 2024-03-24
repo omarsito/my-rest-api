9369320312314daf8cd00c0c6513b0d29999be14
Omar Yamil Abraham López <oysantips@gmail.com>
2024-03-23 19:34:24 -0600

    Improvements for OK_STATUS_MSG cons value

--- src/main/java/com/epam/myrestapi/util/Cons.java
+++ src/main/java/com/epam/myrestapi/util/Cons.java
@@ -4,6 +4,6 @@ public class Cons {
 
     private Cons(){}
 
-    public static final String OK_STATUS_MSG = "My wonderful REST-API is Up & Running within my devops GCP GKE Cluster ...";
+    public static final String OK_STATUS_MSG = "My wonderful REST-API is Up & Running on my GCP GKE Cluster (devops) ...";
 
 }
